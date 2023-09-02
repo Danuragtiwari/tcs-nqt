30f75dc2adb4e3210958c4847484cf69f7342f6c
ANURAG TIWARI <86867000+Danuragtiwari@users.noreply.github.com>
2023-09-02 14:43:58 +0530

    All Done

--- Strings/15.py
+++ Strings/15.py
@@ -1,1 +1,13 @@
 # https://takeuforward.org/data-structure/maximum-occurring-character-in-a-string/
+
+
+s = 'takeuforward'
+max_frequency = 0
+most_common_character = ''
+
+for char in s:
+    frequency = s.count(char)
+    if frequency > max_frequency:
+        max_frequency = frequency
+        most_common_character = char
+print(most_common_character)
